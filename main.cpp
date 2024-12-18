9c27e7d8e64ade7cb270719480fce3bb45ce30b3
RaghavOjha <vindiesel38000@gmail.com>
2024-12-17 23:32:35 -0400

    added init rotor

--- main.cpp
+++ main.cpp
@@ -2,13 +2,33 @@
 using namespace std;
 
 struct rotor {
-    int normal[26];
-    int scrambled[26];
-    int counter;
+    char normal[26];
+    char scrambled[26];
+    int position;
 };
 
+void init_rotor(rotor& x, char config[26]) {
+    // Initialize normal A-Z array
+    for(int i = 0; i < 26; i++) {
+        x.normal[i] = 'A' + i;
+    }
+    // Initialize scrambled array with config
+    for(int i = 0; i < 26; i++) {
+        x.scrambled[i] = config[i];
+    }
+    x.position = 0;
+}
 
-int main(){
-    cout << "hello world";
+int main() {
+    rotor rotor1;
+    char rotor1_config[26] = {'E', 'K', 'M', 'F', 'L', 'G', 'D', 'Q', 'V', 'Z', 'N', 'T', 'O', 'W', 'Y', 'H', 'X', 'U', 'S', 'P', 'A', 'I', 'B', 'R', 'C', 'J'};
+
+    init_rotor(rotor1, rotor1_config);
+    
+    for(int i = 0; i < 26; i++) {
+        cout << rotor1.scrambled[i] << " ";
+    }
+    cout << endl;
+
     return 0;
 }
