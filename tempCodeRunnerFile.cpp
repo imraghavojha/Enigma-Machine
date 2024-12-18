df6b9715784fccfc1c33089e4041e1e05a0abc54
RaghavOjha <vindiesel38000@gmail.com>
2024-12-17 23:04:41 -0400

    made main file

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+struct rotor {
+    int normal[26];
+    int scrambled[26];
+    int counter;
+}
+
+
+int main(){
+    cout << "hello world";
+    return 0;
+}
