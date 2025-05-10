90194c2d9baf7c30fd9f48a61bb8852e031a3f1d
RaghavOjha <jeh267@txstate.edu>
2025-05-10 14:34:55 -0500

    WIP: Added setPosition method in rotor
    also prettier formatted the code so looks like huge change

--- rotor.cpp
+++ rotor.cpp
@@ -1,106 +1,129 @@
 #include <iostream>
-#include <algorithm> 
+#include <algorithm>
 using namespace std;
 
-class rotor{
+class rotor
+{
 
-    private:
-        char normal[26];
-        char scrambled[26];
-        int position;
-        char init_position;
+private:
+    char normal[26];
+    char scrambled[26];
+    int position;
+    char init_position;
 
+public:
+    // constructor
+    rotor(char nor[26], char scram[26], int pos, char init_pos)
+    {
+        std::copy(nor, nor + 26, normal);
+        std::copy(scram, scram + 26, scrambled);
+        position = pos;
+        init_position = init_pos;
+    }
 
-    public:
+    void init_rotor(char config[26], char init_config = 'A')
+    {
 
-        //constructor
-        rotor(char nor[26], char scram[26], int pos, char init_pos){
-            std::copy(nor, nor + 26, normal);
-            std::copy(scram, scram + 26, scrambled);
-            position= pos;
-            init_position= init_pos;
+        // Initialize normal A-Z array
+        for (int i = 0; i < 26; i++)
+        {
+            normal[i] = 'A' + i;
         }
+        // Initialize scrambled array with config
+        for (int i = 0; i < 26; i++)
+        {
+            scrambled[i] = config[i];
+        }
+        position = 0;
+        init_position = init_config; // Store initial position
 
+        while (normal[0] != init_position)
+        { // Fixed: using init_position
+            rotate_rotor();
+        }
+    }
 
-     
-        void init_rotor(char config[26], char init_config = 'A') {
+    int step_through_rotor(int input_position) const
+    {
+        // Forward path (right to left)
+        char letter = scrambled[input_position]; // Get letter from scrambled at position
 
-            // Initialize normal A-Z array
-            for(int i = 0; i < 26; i++) {
-                normal[i] = 'A' + i;
+        // Find this letter's position in next rotor's normal array
+        for (int i = 0; i < 26; i++)
+        {
+            if (letter == normal[i])
+            {
+                return i;
             }
-            // Initialize scrambled array with config
-            for(int i = 0; i < 26; i++) {
-                scrambled[i] = config[i];
+        }
+        return -1;
+    }
+
+    int step_through_rotor_reverse(int input_position) const
+    {
+        // Reverse path (left to right)
+        char letter = normal[input_position]; // Get letter from normal at position
+
+        // Find this letter's position in next rotor's scrambled array
+        for (int i = 0; i < 26; i++)
+        {
+            if (letter == scrambled[i])
+            {
+                return i;
             }
-            position = 0;
-            init_position = init_config;  // Store initial position
+        }
+        return -1;
+    }
 
-            while(normal[0] != init_position) {  // Fixed: using init_position
-                rotate_rotor();
-            }
+    void rotate_column(char y[26])
+    {
+        char first = y[0];
+        for (int i = 0; i < 25; i++)
+        {
+            y[i] = y[i + 1];
+        }
+        y[25] = first;
+    }
+
+    void rotate_rotor()
+    {
+        rotate_column(normal);
+        rotate_column(scrambled);
+        position = (position + 1) % 26;
+    }
+
+    void reset()
+    {
+        while (normal[0] != init_position)
+        {
+            rotate_rotor();
+        }
+    }
+
+    void setPosition(char new_position)
+    {
+        // Calculate how many rotations we need
+        // First, get back to 'A' position
+        while (normal[0] != 'A')
+        {
+            rotate_rotor();
         }
 
-      
-
-        int step_through_rotor(int input_position) const {
-            // Forward path (right to left)
-            char letter = scrambled[input_position];  // Get letter from scrambled at position
-            
-            // Find this letter's position in next rotor's normal array
-            for (int i = 0; i < 26; i++) {
-                if (letter == normal[i]) {
-                    return i;
-                }
-            }
-            return -1;
+        // Then rotate until we reach the desired position
+        while (normal[0] != new_position)
+        {
+            rotate_rotor();
         }
+    }
 
-        int step_through_rotor_reverse(int input_position) const {
-            // Reverse path (left to right)
-            char letter = normal[input_position];  // Get letter from normal at position
-            
-            // Find this letter's position in next rotor's scrambled array
-            for (int i = 0; i < 26; i++) {
-                if (letter == scrambled[i]) {
-                    return i;
-                }
-            }
-            return -1;
-        }
+    // getters
+    int getPosition() const
+    {
+        return position;
+    }
 
-
-        void rotate_column(char y[26]) {  
-            char first = y[0];
-            for(int i = 0; i < 25; i++) {  
-                y[i] = y[i + 1];
-            }
-            y[25] = first;  
-        }
-
-        void rotate_rotor() {
-            rotate_column(normal);
-            rotate_column(scrambled);
-            position = (position + 1) % 26;  
-        }
-
-        void reset(){
-            while(normal[0] != init_position){
-                rotate_rotor();
-            }
-
-        }
-
-        
-        //getters
-        int getPosition() const {
-            return position;
-        }
-
-        char getCurrentLetter() const {
-            return normal[0];
-            //maybe return init_position;
-        }
-    
-
+    char getCurrentLetter() const
+    {
+        return normal[0];
+    }
 };
